0b4f9630eb5a2de5cdb97810a79ec46d3624ffc2
n1rdeks <n1rdeks@mail.ru>
2020-10-10 12:23:35 +0300

    added template for product details

--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -1,11 +1,71 @@
 import React from 'react';
+import products from "../products";
+import {Link} from "react-router-dom";
+import {Button, Card, Col, Image, ListGroup, ListGroupItem, Row} from "react-bootstrap";
+import Rating from "../components/Rating";
 
 
-const ProductScreen = () => {
+const ProductScreen = ({match}) => {
+    const  product = products.find(p => p._id === match.params.id);
+
     return (
-        <div>
-            Product
-        </div>
+        <>
+            <Link className="btn btn-dark my-3" to="/">Go Back</Link>
+            <Row>
+                <Col md={6}>
+                    <Image src={product.image} alt={product.name} fluid />
+                </Col>
+                <Col md={3}>
+                    <ListGroup variant="flush">
+                        <ListGroupItem>
+                            <h3>{product.name}</h3>
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            <Rating text={` ${product.numReviews} reviews`}
+                                    value={product.rating} />
+                        </ListGroupItem>
+                        <ListGroupItem>
+                           Price: ${product.price}
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            Description: {product.description}
+                        </ListGroupItem>
+                    </ListGroup>
+                </Col>
+                <Col md={3}>
+                    <Card>
+                        <ListGroup variant="flush">
+                            <ListGroupItem>
+                                <Row>
+                                    <Col>
+                                        Price:
+                                    </Col>
+                                    <Col>
+                                        <strong>${product.price}</strong>
+                                    </Col>
+                                </Row>
+                            </ListGroupItem>
+                            <ListGroupItem>
+                                <Row>
+                                    <Col>
+                                        Status:
+                                    </Col>
+                                    <Col>
+                                        {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                                    </Col>
+                                </Row>
+                            </ListGroupItem>
+                            <ListGroupItem>
+                                <Button className="btn-block" type="button"
+                                        disabled={product.countInStock === 0}>
+                                    Add to card
+                                </Button>
+                            </ListGroupItem>
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
+        </>
     );
 };
 
