4ec43f9faf451f2c9e8d91d0168c41e13ed5e2bf
n1rdeks <n1rdeks@mail.ru>
2020-10-24 23:08:59 +0300

    added custom error middleware

--- api/server.js
+++ api/server.js
@@ -3,18 +3,22 @@ import dotenv from 'dotenv';
 
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import {notFound, errorHandler} from "./middleware/errorMiddleware.js";
 
 
 dotenv.config();
+
 connectDB();
 
 const PORT = process.env.PORT || 5050;
 
 const  app = express();
 
-
 app.use('/api/products', productRoutes);
 
+app.use(notFound);
+
+app.use(errorHandler);
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
 
